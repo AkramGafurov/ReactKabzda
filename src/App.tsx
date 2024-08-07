7b76797e576f13ff57682bcca2e3f792cc0448f3
AkramGafurov <akramminsk@yandex.ru>
2024-08-07 21:26:13 +0300

    feat: remove logic of components Rating and Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
 function App() {
     return (
         <div className="App">
-            <Rating/>
-            <Accordion/>
+            <Rating value={0}/>
+            <Accordion accordionTitle={'accordion'}/>
         </div>
     );
 }
-
-function Rating() {
-    return (
-        <div>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
-    );
-}
-
-function Star() {
-    return (
-        <div>
-            star
-        </div>
-    );
-}
-
-function AccordionTitle() {
-    return (
-        <div>
-            <h3>Menu</h3>
-        </div>
-    );
-}
-
-function AccordionBody() {
-    return (
-        <div>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </div>
-    );
-}
-
-function Accordion() {
-    return (
-        <div>
-            <AccordionTitle/>
-            <AccordionBody/>
-        </div>
-    );
-}
-
 
 export default App;
