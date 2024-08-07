cc49c35c7ab93b5ce8866f7343b1049184188c5a
AkramGafurov <akramminsk@yandex.ru>
2024-08-07 21:24:40 +0300

    feat: move component Rating from App.tsx to Rating .tsx

---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+type RatingPropsType = {
+    value: number,
+}
+
+export function Rating(props: RatingPropsType) {
+    return (
+        <div>
+            <Star value={props.value} innerValue={1}/>
+            <Star value={props.value} innerValue={2}/>
+            <Star value={props.value} innerValue={3}/>
+            <Star value={props.value} innerValue={4}/>
+            <Star value={props.value} innerValue={5}/>
+        </div>
+    );
+}
+
+type StarPropsType = {
+    innerValue:1|2|3|4|5
+    value: number,
+}
+
+function Star(props: StarPropsType) {
+    return (
+        <div>
+            {props.value>= props.innerValue ?
+                <b>star</b> : <span>star</span>}
+        </div>
+    );
+}
