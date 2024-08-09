411213386fb28a48b085b5a6e6230e3d22963700
AkramGafurov <akramminsk@yandex.ru>
2024-08-09 20:29:07 +0300

    refactor: change logic detecting for Star state in Accordion.tsx

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -8,24 +8,23 @@ type RatingPropsType = {
 export function Rating(props: RatingPropsType) {
     return (
         <div>
-            <Star value={props.value} innerValue={1}/>
-            <Star value={props.value} innerValue={2}/>
-            <Star value={props.value} innerValue={3}/>
-            <Star value={props.value} innerValue={4}/>
-            <Star value={props.value} innerValue={5}/>
+            <Star selected={props.value>0} />
+            <Star selected={props.value>1} />
+            <Star selected={props.value>2} />
+            <Star selected={props.value>3} />
+            <Star selected={props.value>4} />
         </div>
     );
 }
 
 type StarPropsType = {
-    innerValue:1|2|3|4|5
-    value: number,
+    selected: boolean
 }
 
 function Star(props: StarPropsType) {
     return (
         <div>
-            {props.value>= props.innerValue ?
+            {props.selected?
                 <b>star</b> : <span>star</span>}
         </div>
     );
