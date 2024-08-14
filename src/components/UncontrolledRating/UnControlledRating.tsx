8749b7c174093aee07e8ae0cac5d005e825a376a
AkramGafurov <akramminsk@yandex.ru>
2024-08-14 09:35:15 +0300

    feat : change logic of activating star

--- src/components/UncontrolledRating/UnControlledRating.tsx
+++ src/components/UncontrolledRating/UnControlledRating.tsx
@@ -6,23 +6,24 @@ export function UnControlledRating() {
 
     return (
         <div>
-            <Star selected={value > 0} onClick={()=>setValue(1)}/>
-            <Star selected={value > 1} onClick={()=>setValue(2)}/>
-            <Star selected={value > 2} onClick={()=>setValue(3)}/>
-            <Star selected={value > 3} onClick={()=>setValue(4)}/>
-            <Star selected={value > 4} onClick={()=>setValue(5)}/>
+            <Star selected={value > 0} setValue={setValue} value={1}/>
+            <Star selected={value > 1} setValue={setValue} value={2}/>
+            <Star selected={value > 2} setValue={setValue} value={3}/>
+            <Star selected={value > 3} setValue={setValue} value={4}/>
+            <Star selected={value > 4} setValue={setValue} value={5}/>
         </div>
     );
 }
 
 type StarPropsType = {
     selected: boolean
-    onClick: ()=> void
+    value: 1|2|3|4|5
+    setValue: (value:1|2|3|4|5)=> void
 }
 
 function Star(props: StarPropsType) {
     return (
-        <div onClick={props.onClick}>
+        <div onClick={()=>{props.setValue(props.value)}}>
             {props.selected ?
                 <b>star</b> : <span>star</span>}
         </div>
