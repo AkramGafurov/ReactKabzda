a49b531b6f4f66649ff9458220bd6ccf4e0e7407
AkramGafurov <akramminsk@yandex.ru>
2024-10-27 20:26:05 +0300

    feat: finish task useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
-import {Rating} from "./components/Rating/Rating";
+import {Rating, RatingValueType} from "./components/Rating/Rating";
+import {OnOff} from "./components/OnOff/OnOff";
+import UnControlledAccordion from "./components/unControlledAccordion/UnControlledAccordion";
+import {UnControlledRating} from "./components/UncontrolledRating/UnControlledRating";
+// import {UnControlledOnOff} from "./components/UnControlledOnOff/UnControlledOnOff";
 
 function App() {
+    const[accordionCollapsed, setAccordionCollapsed] = useState(false)
+    const[isOn, setIsOn] = useState(true)
+    const[ratingValue, setRatingValue] = useState<RatingValueType>(0)
+
     return (
         <div className="App">
-            <Rating value={0}/>
-            <Accordion titleValue={'accordion'} collapsed={false}/>
-            <Accordion titleValue={'accordion'} collapsed={true}/>
+            <h2>Accordion</h2>
+            <Accordion titleValue={'accordion'} collapsed={accordionCollapsed}
+                       onChange={() => setAccordionCollapsed(!accordionCollapsed)}/>
+            <UnControlledAccordion titleValue={'uncontrolled'}/>
+            <h2>OnOff</h2>
+            {/*<UnControlledOnOff isOn={true}/>*/}
+            {/*<UnControlledOnOff isOn={false}/>*/}
+            <OnOff isOn={isOn} onChange={setIsOn}/>
+            {/*<UnControlledOnOff/>*/}
+
+            <h2>Rating</h2>
+            <h3>Rating</h3>
+            <Rating value={ratingValue} onClick={setRatingValue}/>
+            <h3>UnControlledRating</h3>
+            <UnControlledRating/>
         </div>
     );
 }
---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,96 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {fn} from '@storybook/test';
+import React, {ChangeEvent, ChangeEventHandler, useMemo, useState} from "react";
+import {number} from "prop-types";
+
+export default {
+    title: 'useMemo',
+};
+
+export const DifficultExampleCounting = () => {
+    let [a, setA] = useState<number>(0)
+    let [b, setB] = useState<number>(0)
+
+    const changeHandlerA = (e: ChangeEvent<HTMLInputElement>) => {
+        setA(+e.currentTarget.value)
+    }
+    const changeHandlerB = (e: ChangeEvent<HTMLInputElement>) => {
+        setB(+e.currentTarget.value)
+    }
+    let resultA = 1;
+    let resultB = 1;
+
+
+    resultA = useMemo(() => {
+        let tempResultA = 1
+        for (let i = 1; i <= a; i++) {
+            let fake = 0;
+            while (fake < 100000000) {
+                fake = fake + 1
+                let fakeValue = Math.random()
+            }
+            tempResultA = tempResultA * i
+        }
+        return tempResultA
+    }, [a]);
+
+// for (let i=1; i<=a;i++){
+//     let fake = 0;
+//     while(fake<100000000){
+//         fake = fake+1
+//         let fakeValue = Math.random()
+//     }
+//     resultA = resultA*i
+// }
+
+    for (let i = 1; i <= b; i++) {
+        resultB = resultB * i
+    }
+
+    return <>
+        <input type="text" value={a} onChange={changeHandlerA}/>
+        <input type="text" value={b} onChange={changeHandlerB}/>
+        <div>
+            <div>result a: {resultA}</div>
+            <div>result b: {resultB}</div>
+        </div>
+    </>
+}
+
+
+const UsersSecrets = (props: { users: Array<string> }) => {
+    console.log('UsersSecrets')
+    return <div>
+        {props.users.map((user,i) => <div key={i}>{user}</div>)}
+    </div>
+}
+
+const Users = React.memo(UsersSecrets)
+
+export const HelpToReactMemo = () => {
+
+    console.log('HelpToReactMemo')
+
+    const [counter, setCounter] = useState<number>(0)
+
+    const [users, setUsers] = useState(["Dimych", "Valera", "Artem", "Katya"])
+
+let newArray = useMemo(()=>{
+    let filtredUsers = users.filter(i=>i.toLowerCase().includes('a'))
+    return filtredUsers;
+},[users])
+
+    const addUser = ()=>{
+        setUsers([...users, 'Sveta '+ new Date().getTime()])
+    }
+    return <div>
+        <button onClick={() => {
+            setCounter(counter + 1)
+        }}>+
+        </button>
+        <button onClick={addUser}>AddUser
+        </button>
+        {counter}
+        <Users users={newArray}/>
+    </div>
+}
